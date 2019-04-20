3b18bced02796d26b91d5579969b019521a2a0de
Maxime Bertonnier <m.bertonnier@gmail.com>
2019-04-20 16:54:32 +0200

    test(api): add headers & POST tests

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -9,7 +9,9 @@ const API_OPTIONS = {
 };
 
 const API_SERVICES = {
-  example: { path: 'example.json' }
+  testHeaders: { path: 'testHeaders' },
+  getExample: { path: 'example.json' },
+  postExample: { path: 'postExample', method: 'POST' }
 };
 
 
@@ -23,26 +25,51 @@ describe('>>> Test API calls', () => {
     mockedServer.close(done);
   });
 
+  it('Returns headers', async () => {
+    const api = new APIpeline(API_OPTIONS, API_SERVICES);
+    try {
+      await expect(
+        api.get('testHeaders', { headers: { 'Content-Type': 'application/pdf' } })
+      ).resolves.toHaveProperty('content-type', 'application/pdf');
+    } catch (err) {
+      throw err;
+    }
+  });
+
   it('GET/example.json (api.get)', async () => {
     const api = new APIpeline(API_OPTIONS, API_SERVICES);
-    await expect(api.get('example')).resolves.toEqual(exampleData);
+    await expect(api.get('getExample')).resolves.toEqual(exampleData);
   });
 
   it('GET/example.json (api.fetch)', async () => {
     const api = new APIpeline(API_OPTIONS, API_SERVICES);
-    await expect(api.fetch('example')).resolves.toEqual(exampleData);
+    await expect(api.fetch('getExample')).resolves.toEqual(exampleData);
   });
 
   it('GET/example.json (api.getHeaders)', async () => {
     const api = new APIpeline(API_OPTIONS, API_SERVICES);
     expect.assertions(2);
     try {
-      const headers = await api.fetchHeaders('example');
+      const headers = await api.fetchHeaders('getExample');
       expect(headers).toHaveProperty('content-type');
       expect(headers['content-type']).toEqual('application/json; charset=UTF-8');
     } catch (err) {
       throw err;
     }
   });
-});
 
+  it('POST/postExample (api.post)', async () => {
+    const api = new APIpeline(API_OPTIONS, API_SERVICES);
+    try {
+      await expect(api.post(
+        'postExample',
+        {
+          fetchOptions: { body: 'key=value' },
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+        }
+      )).resolves.toEqual({ key: 'value' });
+    } catch (err) {
+      throw err;
+    }
+  });
+});
